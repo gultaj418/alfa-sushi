5329cb897c4667f9e5e74f81d3fd7ab252b83518
azadsarxanli <azad.sarxanli@gmail.com>
2022-01-25 23:40:40 +0400

    "responsive.css changes, style.css changes, index.js changes, index.html changes"

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //defining variables
+
 const search = document.querySelector("#search");
 const searchBar = document.querySelector(".search-bar");
 const plusOrder = document.querySelectorAll(".plus");
@@ -6,19 +7,30 @@ const minusOrder = document.querySelectorAll(".minus");
 const langChanger = document.querySelector("#lang");
 const langSection = document.querySelector(".language-section");
 const svgRotator = document.querySelector(".svg-class");
-// const languageSection = document.querySelectorAll(".language-section");
+const languages = document.querySelectorAll(".language");
 const hamburger = document.querySelector(".hamburger-menu");
 const blackMobileMenu = document.querySelector(".black-menu");
 const bagCounter = document.querySelector("#bag");
 const downIcon = document.querySelector(".arrow-down-icon");
 const upIcon = document.querySelector(".arrow-up-icon");
-
-const x = window.matchMedia("(max-width: 768px)");
+const upIconOffset = upIcon.offsetHeight;
+let hamburgerHeavy = document.querySelector(".hamburger-heavy");
+let mobileHeavy = document.querySelector(".mobile-heavy");
+let mql = window.matchMedia("(max-width: 768px)");
 
 //haburger menu
 hamburger.addEventListener("click", () => {
-  console.log(hamburger);
-  blackMobileMenu.classList.toggle("active");
+  blackMobileMenu.classList.add("btn-active");
+  mobileHeavy.classList.add("heavy-background");
+  hamburgerHeavy.classList.add("btn-active");
+  document.body.style.overflowY = ["hidden"];
+});
+
+hamburgerHeavy.addEventListener("click", () => {
+  blackMobileMenu.classList.remove("btn-active");
+  mobileHeavy.classList.remove("heavy-background");
+  hamburgerHeavy.classList.remove("btn-active");
+  document.body.style.overflowY = ["scroll"];
 });
 
 //Language changer
@@ -38,14 +50,19 @@ langChanger.addEventListener("click", () => {
 
 //EventListeners
 search.addEventListener("click", () => {
+  let searchInput = searchBar.querySelector(".search-input");
   search.classList.add("deactive");
   searchBar.classList.add("active");
+  searchInput.classList.add("active");
 });
 searchBar.addEventListener(
   "focusout",
   () => {
+    let searchInput = searchBar.querySelector(".search-input");
+
     search.classList.remove("deactive");
     searchBar.classList.remove("active");
+    searchInput.classList.remove("active");
   },
   false
 );
@@ -102,42 +119,37 @@ minusOrder.forEach(function (el) {
   });
 });
 
-
 //slider responsive
 
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-
-    margin: 40,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    $(".owl-carousel").owlCarousel({
+      margin: 40,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        },
       },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 3,
+    });
+  }
+});
 
+var owl = $("ul.owl-carousel");
+owl.owlCarousel({
+  items: 5,
 
-        
-      },
-      600: {
-        items: 1,
-
-       
-      },
-      1000: {
-        items: 3,
-
-       
-
-      },
-    },
-  });
+  loop: true,
+  // margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
 });
 
 //smooth beahvior scroll
@@ -157,12 +169,11 @@ upIcon.addEventListener("click", () => {
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 100 && window.scrollY < 1800) {
-    downIcon.classList.add("btn-active");
+    downIcon.classList.add("active");
   } else if (
     window.scrollY == 0 ||
     (window.scrollY > 1850 && window.scrollY < 1900)
   ) {
-    downIcon.classList.remove("btn-active");
-
+    downIcon.classList.remove("active");
   }
 });
